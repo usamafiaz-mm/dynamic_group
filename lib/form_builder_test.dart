20d4d40a6d74566e8a6bd2d7821a9d6b2cb32b6f
usamafiaz-mm <u.fayyaz@pk.micromerger.com>
2022-11-14 16:32:31 +0500

    init commit

---
+++ lib/form_builder_test.dart
@@ -0,0 +1,65 @@
+import 'package:reactive_forms/reactive_forms.dart';
+
+class MyFormBuilder {
+  final FormGroup root = fb.group({});
+  List<Map<String, dynamic>> list = [];
+  static int id = 0;
+
+  FormGroup init() {
+    final name = FormControl(
+        value: "John Doe",
+        metaData: MyMetaData(
+            name: "usama",
+            label: "String",
+            required: true,
+            type: "Text",
+            id: 12,
+            validators: {"val": 1}));
+    // name.setMetadataData();
+    addControl("name", name, root);
+    final age = FormControl(value: 19);
+    addControl("age", age, root);
+    final address = FormGroup({});
+    addControl("address", address, root);
+    final city = FormControl(value: "Islamabad");
+    addControl("city", city, address);
+    final zip = FormControl(value: 44000);
+    addControl("zip", zip, address);
+    final province = FormControl(value: "Capital");
+    addControl("province", province, address);
+    final orders = FormArray([]);
+    addControl("orders", orders, root);
+    final anonymous = FormGroup({});
+    addControlFormArray(anonymous, orders);
+    final orderId = FormControl(value: 1);
+    addControl("orderId", orderId, anonymous);
+    final total = FormControl(value: 20);
+    addControl("total", total, anonymous);
+    final anonymous2 = FormGroup({});
+    addControlFormArray(anonymous2, orders);
+    final orderId2 = FormControl(value: 2);
+    addControl("orderId", orderId2, anonymous2);
+    final total2 = FormControl(value: 40);
+    addControl("total", total2, anonymous2);
+    final gender = FormControl(value: 0);
+    addControl("gender", gender, root);
+    return root;
+  }
+
+  void addControl(String name, dynamic control, FormGroup parent) {
+    parent.addAll({name: control});
+    list.add({"id": id, "reference": control});
+    id += 1;
+  }
+
+  void addControlFormArray(dynamic control, FormArray parent) {
+    parent.add(control);
+    list.add({"id": id, "reference": control});
+    id += 1;
+  }
+
+  getById(int id) {
+    final object = list.firstWhere((element) => element["id"] == id);
+    return object["reference"];
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+import 'form_builder_test.dart';
+
+void main() {
+  MyFormBuilder formBuilder = MyFormBuilder();
+  formBuilder.init();
+  // final order = formBuilder.getById(11) as FormControl;
+  final order = formBuilder.getById(0) as FormControl;
+  order.myMetaData;
+  order.updateValue('3');
+  print((order.value));
+
+  final gender = formBuilder.getById(11) as FormControl;
+  print((gender.value));
+  print(formBuilder.root.value);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late FormControl gender;
+  late FormGroup root;
+
+  @override
+  void initState() {
+    MyFormBuilder formBuilder = MyFormBuilder();
+    root = formBuilder.init();
+    // gender = formBuilder.getById(11);
+    // gender.valueChanges.listen((event) {
+    //   print(event);
+    // });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ReactiveForm(
+          formGroup: root,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 28.0),
+                  child: Text(
+                    "Gender",
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ),
+                ReactiveRadioListTile(
+                  value: 0,
+                  formControl: root.control("gender") as FormControl<int>,
+                  title: Text("Male"),
+                ),
+                ReactiveRadioListTile(
+                    value: 1,
+                    formControl: root.control("gender") as FormControl<int>,
+                    title: Text("Female")),
+                ReactiveTextField(
+                  decoration: InputDecoration(label: Text("Name")),
+                  formControl: root.control("name") as FormControl<String>,
+                ),
+                ReactiveTextField(
+                  decoration: InputDecoration(label: Text("Age")),
+                  formControl: root.control("age") as FormControl,
+                ),
+                ReactiveTextField(
+                  decoration: InputDecoration(label: Text("City")),
+                  formControl: root.control("address.city") as FormControl,
+                ),
+                ReactiveTextField(
+                  decoration: InputDecoration(label: Text("Zip")),
+                  formControl: root.control("address.zip") as FormControl,
+                ),
+                ReactiveTextField(
+                  decoration: InputDecoration(label: Text("Province")),
+                  formControl: root.control("address.province") as FormControl,
+                ),
+                ReactiveTextField(
+                    decoration: InputDecoration(label: Text("Order ID")),
+                    formControl: ((root.control("orders") as FormArray)
+                            .controls[0] as FormGroup)
+                        .control("orderId") as FormControl),
+                ReactiveTextField(
+                    decoration: InputDecoration(label: Text("Total")),
+                    formControl: ((root.control("orders") as FormArray)
+                            .controls[1] as FormGroup)
+                        .control("total") as FormControl),
+                ReactiveTextField(
+                    decoration: InputDecoration(label: Text("Order ID")),
+                    formControl: ((root.control("orders") as FormArray)
+                            .controls[0] as FormGroup)
+                        .control("orderId") as FormControl),
+                ReactiveTextField(
+                    decoration: InputDecoration(label: Text("Total")),
+                    formControl: ((root.control("orders") as FormArray)
+                            .controls[1] as FormGroup)
+                        .control("total") as FormControl),
+                ElevatedButton(
+                    onPressed: () {
+                      print(root.value);
+                    },
+                    child: Text(" Print Value"))
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      ),
+    );
+  }
+}
